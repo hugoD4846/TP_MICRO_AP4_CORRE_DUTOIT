b2610b314c02db1cfc4ce759d07368eb9954730c
leooz <leo59coc@gmail.com>
2023-01-13 11:58:14 +0100

    ~1 sec

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -6,5 +6,5 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=TP_MICRO_AP4_CORRE_DUTOIT.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/TP_MICRO_AP4_CORRE_DUTOIT.X.production.hex
+CND_ARTIFACT_NAME_default=TP_MICRO_AP4_CORRE_DUTOIT.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/TP_MICRO_AP4_CORRE_DUTOIT.production.hex
