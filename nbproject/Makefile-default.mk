4841b86d1a0bde7c2278af08917f1b3dea59cc97
hdutoit <hdutoit@ineat.fr>
2023-02-03 09:05:16 +0100

    mon pote

--- nbproject/Makefile-default.mk
+++ nbproject/Makefile-default.mk
@@ -19,7 +19,7 @@ endif
 endif
 
 # Environment
-MKDIR=gnumkdir -p
+MKDIR=mkdir -p
 RM=rm -f 
 MV=mv 
 CP=cp 
@@ -30,12 +30,12 @@ ifeq ($(TYPE_IMAGE), DEBUG_RUN)
 IMAGE_TYPE=debug
 OUTPUT_SUFFIX=elf
 DEBUGGABLE_SUFFIX=elf
-FINAL_IMAGE=${DISTDIR}/TP_MICRO_AP4_CORRE_DUTOIT.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+FINAL_IMAGE=${DISTDIR}/TP_MICRO_AP4_CORRE_DUTOIT.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
 else
 IMAGE_TYPE=production
 OUTPUT_SUFFIX=hex
 DEBUGGABLE_SUFFIX=elf
-FINAL_IMAGE=${DISTDIR}/TP_MICRO_AP4_CORRE_DUTOIT.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+FINAL_IMAGE=${DISTDIR}/TP_MICRO_AP4_CORRE_DUTOIT.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
 endif
 
 ifeq ($(COMPARE_BUILD), true)
@@ -51,17 +51,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=main.c TP_Chenillard.c OscilateurOneSec.c TP_Chenillard_interruption.c btnInit.c TP_Buzzer_PWM.c
+SOURCEFILES_QUOTED_IF_SPACED=main.c TP_Chenillard.c OscilateurOneSec.c TP_Chenillard_interruption.c btnInit.c librairies_utiles/C_Source_Files/lcd.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/main.p1 ${OBJECTDIR}/TP_Chenillard.p1 ${OBJECTDIR}/OscilateurOneSec.p1 ${OBJECTDIR}/TP_Chenillard_interruption.p1 ${OBJECTDIR}/btnInit.p1 ${OBJECTDIR}/TP_Buzzer_PWM.p1
-POSSIBLE_DEPFILES=${OBJECTDIR}/main.p1.d ${OBJECTDIR}/TP_Chenillard.p1.d ${OBJECTDIR}/OscilateurOneSec.p1.d ${OBJECTDIR}/TP_Chenillard_interruption.p1.d ${OBJECTDIR}/btnInit.p1.d ${OBJECTDIR}/TP_Buzzer_PWM.p1.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/main.p1 ${OBJECTDIR}/TP_Chenillard.p1 ${OBJECTDIR}/OscilateurOneSec.p1 ${OBJECTDIR}/TP_Chenillard_interruption.p1 ${OBJECTDIR}/btnInit.p1 ${OBJECTDIR}/librairies_utiles/C_Source_Files/lcd.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/main.p1.d ${OBJECTDIR}/TP_Chenillard.p1.d ${OBJECTDIR}/OscilateurOneSec.p1.d ${OBJECTDIR}/TP_Chenillard_interruption.p1.d ${OBJECTDIR}/btnInit.p1.d ${OBJECTDIR}/librairies_utiles/C_Source_Files/lcd.p1.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/main.p1 ${OBJECTDIR}/TP_Chenillard.p1 ${OBJECTDIR}/OscilateurOneSec.p1 ${OBJECTDIR}/TP_Chenillard_interruption.p1 ${OBJECTDIR}/btnInit.p1 ${OBJECTDIR}/TP_Buzzer_PWM.p1
+OBJECTFILES=${OBJECTDIR}/main.p1 ${OBJECTDIR}/TP_Chenillard.p1 ${OBJECTDIR}/OscilateurOneSec.p1 ${OBJECTDIR}/TP_Chenillard_interruption.p1 ${OBJECTDIR}/btnInit.p1 ${OBJECTDIR}/librairies_utiles/C_Source_Files/lcd.p1
 
 # Source Files
-SOURCEFILES=main.c TP_Chenillard.c OscilateurOneSec.c TP_Chenillard_interruption.c btnInit.c TP_Buzzer_PWM.c
+SOURCEFILES=main.c TP_Chenillard.c OscilateurOneSec.c TP_Chenillard_interruption.c btnInit.c librairies_utiles/C_Source_Files/lcd.c
 
 
 
@@ -82,7 +82,7 @@ FIXDEPS=fixDeps
 ifneq ($(INFORMATION_MESSAGE), )
 	@echo $(INFORMATION_MESSAGE)
 endif
-	${MAKE}  -f nbproject/Makefile-default.mk ${DISTDIR}/TP_MICRO_AP4_CORRE_DUTOIT.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+	${MAKE}  -f nbproject/Makefile-default.mk ${DISTDIR}/TP_MICRO_AP4_CORRE_DUTOIT.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
 
 MP_PROCESSOR_OPTION=18F46K22
 # ------------------------------------------------------------------------------------
@@ -128,13 +128,13 @@ ${OBJECTDIR}/btnInit.p1: btnInit.c  nbproject/Makefile-${CND_CONF}.mk
 	@-${MV} ${OBJECTDIR}/btnInit.d ${OBJECTDIR}/btnInit.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/btnInit.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
-${OBJECTDIR}/TP_Buzzer_PWM.p1: TP_Buzzer_PWM.c  nbproject/Makefile-${CND_CONF}.mk 
-	@${MKDIR} "${OBJECTDIR}" 
-	@${RM} ${OBJECTDIR}/TP_Buzzer_PWM.p1.d 
-	@${RM} ${OBJECTDIR}/TP_Buzzer_PWM.p1 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1  -mdebugger=pickit4   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/TP_Buzzer_PWM.p1 TP_Buzzer_PWM.c 
-	@-${MV} ${OBJECTDIR}/TP_Buzzer_PWM.d ${OBJECTDIR}/TP_Buzzer_PWM.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/TP_Buzzer_PWM.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+${OBJECTDIR}/librairies_utiles/C_Source_Files/lcd.p1: librairies_utiles/C_Source_Files/lcd.c  nbproject/Makefile-${CND_CONF}.mk 
+	@${MKDIR} "${OBJECTDIR}/librairies_utiles/C_Source_Files" 
+	@${RM} ${OBJECTDIR}/librairies_utiles/C_Source_Files/lcd.p1.d 
+	@${RM} ${OBJECTDIR}/librairies_utiles/C_Source_Files/lcd.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1  -mdebugger=pickit4   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/librairies_utiles/C_Source_Files/lcd.p1 librairies_utiles/C_Source_Files/lcd.c 
+	@-${MV} ${OBJECTDIR}/librairies_utiles/C_Source_Files/lcd.d ${OBJECTDIR}/librairies_utiles/C_Source_Files/lcd.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/librairies_utiles/C_Source_Files/lcd.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 else
 ${OBJECTDIR}/main.p1: main.c  nbproject/Makefile-${CND_CONF}.mk 
@@ -177,13 +177,13 @@ ${OBJECTDIR}/btnInit.p1: btnInit.c  nbproject/Makefile-${CND_CONF}.mk
 	@-${MV} ${OBJECTDIR}/btnInit.d ${OBJECTDIR}/btnInit.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/btnInit.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
-${OBJECTDIR}/TP_Buzzer_PWM.p1: TP_Buzzer_PWM.c  nbproject/Makefile-${CND_CONF}.mk 
-	@${MKDIR} "${OBJECTDIR}" 
-	@${RM} ${OBJECTDIR}/TP_Buzzer_PWM.p1.d 
-	@${RM} ${OBJECTDIR}/TP_Buzzer_PWM.p1 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/TP_Buzzer_PWM.p1 TP_Buzzer_PWM.c 
-	@-${MV} ${OBJECTDIR}/TP_Buzzer_PWM.d ${OBJECTDIR}/TP_Buzzer_PWM.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/TP_Buzzer_PWM.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+${OBJECTDIR}/librairies_utiles/C_Source_Files/lcd.p1: librairies_utiles/C_Source_Files/lcd.c  nbproject/Makefile-${CND_CONF}.mk 
+	@${MKDIR} "${OBJECTDIR}/librairies_utiles/C_Source_Files" 
+	@${RM} ${OBJECTDIR}/librairies_utiles/C_Source_Files/lcd.p1.d 
+	@${RM} ${OBJECTDIR}/librairies_utiles/C_Source_Files/lcd.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/librairies_utiles/C_Source_Files/lcd.p1 librairies_utiles/C_Source_Files/lcd.c 
+	@-${MV} ${OBJECTDIR}/librairies_utiles/C_Source_Files/lcd.d ${OBJECTDIR}/librairies_utiles/C_Source_Files/lcd.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/librairies_utiles/C_Source_Files/lcd.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 endif
 
@@ -202,15 +202,15 @@ endif
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: link
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
-${DISTDIR}/TP_MICRO_AP4_CORRE_DUTOIT.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk    
+${DISTDIR}/TP_MICRO_AP4_CORRE_DUTOIT.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk    
 	@${MKDIR} ${DISTDIR} 
-	${MP_CC} $(MP_EXTRA_LD_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -Wl,-Map=${DISTDIR}/TP_MICRO_AP4_CORRE_DUTOIT.${IMAGE_TYPE}.map  -D__DEBUG=1  -mdebugger=pickit4  -DXPRJ_default=$(CND_CONF)  -Wl,--defsym=__MPLAB_BUILD=1   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto        $(COMPARISON_BUILD) -Wl,--memorysummary,${DISTDIR}/memoryfile.xml -o ${DISTDIR}/TP_MICRO_AP4_CORRE_DUTOIT.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}     
-	@${RM} ${DISTDIR}/TP_MICRO_AP4_CORRE_DUTOIT.${IMAGE_TYPE}.hex 
+	${MP_CC} $(MP_EXTRA_LD_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -Wl,-Map=${DISTDIR}/TP_MICRO_AP4_CORRE_DUTOIT.X.${IMAGE_TYPE}.map  -D__DEBUG=1  -mdebugger=pickit4  -DXPRJ_default=$(CND_CONF)  -Wl,--defsym=__MPLAB_BUILD=1   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto        $(COMPARISON_BUILD) -Wl,--memorysummary,${DISTDIR}/memoryfile.xml -o ${DISTDIR}/TP_MICRO_AP4_CORRE_DUTOIT.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}     
+	@${RM} ${DISTDIR}/TP_MICRO_AP4_CORRE_DUTOIT.X.${IMAGE_TYPE}.hex 
 	
 else
-${DISTDIR}/TP_MICRO_AP4_CORRE_DUTOIT.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk   
+${DISTDIR}/TP_MICRO_AP4_CORRE_DUTOIT.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk   
 	@${MKDIR} ${DISTDIR} 
-	${MP_CC} $(MP_EXTRA_LD_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -Wl,-Map=${DISTDIR}/TP_MICRO_AP4_CORRE_DUTOIT.${IMAGE_TYPE}.map  -DXPRJ_default=$(CND_CONF)  -Wl,--defsym=__MPLAB_BUILD=1   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto     $(COMPARISON_BUILD) -Wl,--memorysummary,${DISTDIR}/memoryfile.xml -o ${DISTDIR}/TP_MICRO_AP4_CORRE_DUTOIT.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}     
+	${MP_CC} $(MP_EXTRA_LD_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -Wl,-Map=${DISTDIR}/TP_MICRO_AP4_CORRE_DUTOIT.X.${IMAGE_TYPE}.map  -DXPRJ_default=$(CND_CONF)  -Wl,--defsym=__MPLAB_BUILD=1   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto     $(COMPARISON_BUILD) -Wl,--memorysummary,${DISTDIR}/memoryfile.xml -o ${DISTDIR}/TP_MICRO_AP4_CORRE_DUTOIT.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}     
 	
 endif
 
@@ -230,7 +230,7 @@ endif
 # Enable dependency checking
 .dep.inc: .depcheck-impl
 
-DEPFILES=$(shell mplabwildcard ${POSSIBLE_DEPFILES})
+DEPFILES=$(shell "${PATH_TO_IDE_BIN}"mplabwildcard ${POSSIBLE_DEPFILES})
 ifneq (${DEPFILES},)
 include ${DEPFILES}
 endif
